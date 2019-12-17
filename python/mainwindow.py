18c1e789e93d84fa0f582f3cd512426b17321955
unknown <montaser_12@hotmail.com>
2019-12-17 10:39:33 +0200

    generic

--- python/mainwindow.py
+++ python/mainwindow.py
@@ -3,8 +3,14 @@ import sys
 
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
-
+from PyQt5.QtGui import QPixmap
+import threading
+import time
+import requests
+import base64
 # from saftey import Ui_MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 from newgui import Ui_MainWindow
 
 
@@ -13,10 +19,57 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.images = {"ee" : "Elec.png"}
+        self.tread_keep_alive = True
+        self.counter = 0
+        self.add_images_to_list()
+        self.ui.pushButton.clicked.connect(self.show_button)
+        x = threading.Thread(target=self.thread_function, args=())
+        x.start()
 
     def closeEvent(self, event):
-        self.ui.kill_thread()
+        self.kill_thread()
         event.accept()
+
+    def add_images_to_list(self):
+        for key,val in self.images.items():
+            self.ui.listWidget.addItem(key)
+
+    def show_button(self):
+        # print(self.listWidget.currentItem().text())
+        pixmap = QPixmap(self.images[self.ui.listWidget.currentItem().text()])
+
+        pixmap = pixmap.scaled(300, 200, QtCore.Qt.KeepAspectRatio)
+        self.ui.label.setPixmap(pixmap)
+
+
+    def add_image(self,imgname,img):
+        self.images[imgname] = img
+        self.ui.listWidget.addItem(imgname)
+        print("image added from server")
+
+    def thread_function(self):
+        print("sd")
+        while self.tread_keep_alive:
+            try:
+                response = requests.get("http://10.144.66.31:5000/newPic")
+                if response.text == "true":
+                    response = requests.get("http://10.144.66.31:5000/getImage")
+                    data = response.json()
+                    print(data)
+                    pm = QtGui.QPixmap()
+                    decoded = base64.b64decode(data["image"])
+                    # print(decoded)
+                    pm.loadFromData(decoded)
+                    self.add_image(f'Time: {data["time"]} , Location: {data["location"]}',pm)
+                    self.counter += 1
+                time.sleep(2)
+            except:
+                pass
+
+    def kill_thread(self):
+        self.tread_keep_alive = False
+
 
 
 
--- python/newgui.py
+++ python/newgui.py
@@ -17,10 +17,6 @@ import base64
 
 
 class Ui_MainWindow(object):
-    def __init__(self):
-        self.images = {"elictric" : "Elec.png" , "Fire" : "FireAttack.jpg"}
-        self.tread_keep_alive = True
-        self.counter = 0
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -37,12 +33,8 @@ class Ui_MainWindow(object):
         self.listWidget.setObjectName("listWidget")
         self.verticalLayout_2.addWidget(self.listWidget)
 
-        self.add_images_to_list()
-
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setObjectName("pushButton")
-
-        self.pushButton.clicked.connect(self.show_button)
 
         self.verticalLayout_2.addWidget(self.pushButton)
         self.label = QtWidgets.QLabel(self.centralwidget)
@@ -59,44 +51,12 @@ class Ui_MainWindow(object):
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        x = threading.Thread(target=self.thread_function, args=())
-        x.start()
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.pushButton.setText(_translate("MainWindow", "show"))
 
-    def add_images_to_list(self):
-        for key,val in self.images.items():
-            self.listWidget.addItem(key)
-
-    def show_button(self):
-        # print(self.listWidget.currentItem().text())
-        pixmap = QPixmap(self.images[self.listWidget.currentItem().text()])
-
-        pixmap = pixmap.scaled(300, 200, QtCore.Qt.KeepAspectRatio)
-        self.label.setPixmap(pixmap)
 
 
-    def add_image(self,imgname,img):
-        self.images[imgname] = img
-        self.listWidget.addItem(imgname)
-        print("image added from server")
-
-    def thread_function(self):
-        while self.tread_keep_alive:
-            response = requests.get("http://10.144.66.31:5000/newPic")
-            if response.text == "true":
-                response = requests.get("http://10.144.66.31:5000/getImage")
-                pm = QtGui.QPixmap()
-                decoded = base64.b64decode(response.text)
-                print(decoded)
-                pm.loadFromData(decoded)
-                self.add_image("image " + str(self.counter),pm)
-                self.counter += 1
-            time.sleep(5)
-
-    def kill_thread(self):
-        self.tread_keep_alive = False
 
